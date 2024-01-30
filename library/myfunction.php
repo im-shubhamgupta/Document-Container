a71a31cf5fc47d7c9a05c48c516564ca6edf643f
Shubham kumar Gupta <shubhamgupta309@gmail.com>
2024-01-30 20:33:05 +0530

    make layout for docucments

--- library/myfunction.php
+++ library/myfunction.php
@@ -22,9 +22,14 @@ function redirect($r,$response){
 	header('location:'.SITE_URL.'?action='.$r);
 	die;
 }
-
 function sessionflush($msg){
 	$_SESSION['msg'] = $msg;
+}
+function sessionFlash(){
+	if(isset($_SESSION['flash'])){
+		echo $_SESSION['flash']['msg'];
+		unset($_SESSION['flash']);
+	}
 }
 function sessionClear($msg=''){
 	if(!empty($msg)){
